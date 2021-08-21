1fcdc25b9f44d10e63ad797a086c5ced174b7e3d
nonthawat laephua <nonthawat3944@gmail.com>
2021-08-21 12:06:11 +0700

    Update index.php

--- admin/pages/dashboard/index.php
+++ admin/pages/dashboard/index.php
@@ -245,7 +245,7 @@ require_once('../../services/session.php');
             <div class="col-md-12 mb-3">
                 <div class="card">
                     <div class="card-header">
-                        <span><i class="bi bi-table me-2"></i></span> Data Table
+                        <span><i class="bi bi-table me-2"></i></span> รายงานยอดรายเดือน
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
